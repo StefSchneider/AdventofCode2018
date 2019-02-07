1cef704709e670a2a733bf22d27abbea19fb46f5
schne <schneider.solingen@web.de>
2019-02-07 21:44:50 +0100

    Day 02 - just part one

--- AoC2018_02.py
+++ AoC2018_02.py
@@ -1,14 +1,14 @@
-'''
+"""
 Advent of Code 2018 - Day 2
 Author: Stefan Schneider
 github stefschneider1970
-'''
+"""
 
 import operator
 from collections import Counter
 from itertools import cycle # only for Part 2
 
-path_file: str = 'AoC2018_02_data_id.txt'
+path_file: str = "AoC2018_02_input.txt"
 count_double_elements: int = 0
 count_triple_elements: int = 0
 check_double_elements: bool = False
@@ -18,9 +18,9 @@ checksum: int = 0
 
 # ---------- PART ONE ----------
 
-print('Starting part one...\r')
+print("Starting part one...\r")
 
-with open(path_file, 'r') as data_file:
+with open(path_file, "r") as data_file:
     # use file with data for ids
     for line in data_file:
         line = line.strip()
@@ -34,12 +34,12 @@ with open(path_file, 'r') as data_file:
             count_triple_elements += 1
             check_triple_elements = True
 
-print('Found double elements: ', count_double_elements)
-print('Found triple elements: ', count_triple_elements)
-print('Checksum: ', count_double_elements*count_triple_elements)
-print('\r')
+print("Found double elements: ", count_double_elements)
+print("Found triple elements: ", count_triple_elements)
+print("Checksum: ", count_double_elements * count_triple_elements)
+print("\r")
 
 
 # ---------- PART TWO ----------
 
-print('Starting part two...\r')
+print("Starting part two...\r")
