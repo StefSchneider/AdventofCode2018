48e7b1543d66224aea9e7e3ebbde9b31727e5738
schne <schneider.solingen@web.de>
2019-02-07 21:21:09 +0100

    Day 01 - final solution

--- AoC2018_01.py
+++ AoC2018_01.py
@@ -1,17 +1,17 @@
-'''
+"""
 Advent of Code 2018 - Day 1
 Author: Stefan Schneider
 Github stefschneider1970
-'''
+"""
 
 from itertools import cycle # just for Part 2
 
-path_file: str = 'AoC2018_01_data_calibration.txt'
+path_file: str = "AoC2018_01_input.txt"
 
 
 # ---------- PART ONE ----------
 
-print('Starting part one...\r')
+print("Starting part one...\r")
 
 resulting_frequency: int = 0
 
@@ -20,18 +20,18 @@ with open(path_file, 'r') as data_file:
     for line in data_file:
         resulting_frequency += int(line.strip())
 
-print('Resulting frequency: ',resulting_frequency)
-print('\r')
+print("Resulting frequency: ", resulting_frequency)
+print("\r")
 
 
 # ---------- PART TWO ----------
 
-print('Starting part two...\r')
+print("Starting part two...\r")
 
 frequency: int = 0
 frequencies: set = set((frequency,)) # creates a tuple of frequencies
 
-with open(path_file, 'r') as data_file:
+with open(path_file, "r") as data_file:
     # use file with data for frequencies
     for line in cycle(data_file):
         frequency += int(line)
@@ -39,4 +39,4 @@ with open(path_file, 'r') as data_file:
             break # stop cycle if first double frequency
         frequencies.add(frequency)
 
-print('First double frequency:', frequency)
+print("First double frequency:", frequency)
