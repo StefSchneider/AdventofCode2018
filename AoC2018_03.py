431ce5e289a2fd3948cd7473f9a7d4fff5212fca
schne <schneider.solingen@web.de>
2019-02-10 16:35:49 +0100

    Day 03 - final solution complete

--- AoC2018_03.py
+++ AoC2018_03.py
@@ -4,7 +4,6 @@ Author: Stefan Schneider
 Github: StefSchneider
 """
 
-
 path_file: str = 'AoC2018_03_input.txt'
 id: str = ''
 coordinates_size: str = ''
@@ -14,44 +13,50 @@ x_pos: int = 0 # part of coordinates
 y_pos: int = 0 # part of coordinates
 x_size: int = 0 # part of size
 y_size: int = 0 # part of size
-list_claims: list = []
+claims = set()
 overlap: int = 0
-set_ids = set()
+set_ids: set = set()
 
-fabric = [[list_claims[:] for j in range(1000)] for i in range(1000)]
+fabric = [[set(claims) for j in range(1000)] for i in range(1000)] # initialize 1000x1000 matrix
 
-with open(path_file, 'r') as data_file:
-    for line in data_file:
+with open(path_file, "r") as data_file:
+    print("Adding ids to claim sets...\r")
+    for line in data_file: # split data of each line
+        # use file with data for claims
         id, coordinates_size = line.strip().split(' @ ')
         set_ids.add(id)
         coordinates, size = coordinates_size.strip().split(': ')
         x_pos, y_pos = coordinates.strip().split(',')
         x_size, y_size = size.strip().split('x')
-        print('ID: ', id, 'Positions: ', x_pos, y_pos, 'Size: ', x_size, y_size)
         x_size = int(x_size)
         y_size = int(y_size)
         x_pos = int(x_pos)
         y_pos = int(y_pos)
         for count_x in range(0, x_size):
             for count_y in range(0, y_size):
-                fabric[x_pos+count_x][y_pos+count_y].append(id)
-#                print(x_pos+count_x, y_pos+count_y)
- #               print(fabric[x_pos+count_x][y_pos+count_y])
+                fabric[x_pos+count_x][y_pos+count_y].add(id)
 
 
-for i in range(0, 1000):
+# ---------- PART ONE ----------
+
+print("Starting part one...\r")
+print("Count overlapping inches..\r")
+
+for i in range(0, 1000): # count square inches used by more than one claims
     for j in range(0, 1000):
         if len(fabric[i][j]) > 1:
             overlap += 1
-#            print(len(fabric[i][j]), fabric[i][j])
-#            print(overlap)
+print("Result of part one:", overlap, "inches are overlapping.")
+print("\r")
 
-print(len(set_ids), set_ids)
 
-for i in range(0, 1000):
+# ---------- PART TWO ----------
+
+print("Starting part two...\r")
+print("Deleting ids of inches used by more than one claim...\r")
+
+for i in range(1, 1000):
     for j in range(0, 1000):
         if len(fabric[i][j]) > 1:
-            for elements in fabric[i][j]:
-                set_ids.discard(elements)
-
-print(len(set_ids), set_ids)
+            set_ids.difference_update(fabric[i][j])
+print("Result of part two:", list(set_ids)[0])
