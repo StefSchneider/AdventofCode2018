e65481634630ff8f235e00dc133cd7c835b5373f
schne <schneider.solingen@web.de>
2019-02-10 23:31:11 +0100

    Day 9

--- AoC2018_09.py
+++ AoC2018_09.py
@@ -4,8 +4,15 @@ Author: Stefan Schneider
 Github: StefSchneider
 """
 
-from collections import deque
+import collections
 
 players:int = 452
 last_marble_point: int = 70784
+player: int = 1
 
+
+players_points = collections.defaultdict()
+marbles = collections.deque([0])
+
+
+print(marbles[0])
