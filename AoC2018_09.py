081441bcc252ffb9dd8198e959eb03c4c169b4f3
schne <schneider.solingen@web.de>
2019-02-11 08:31:25 +0100

    Day 9 -

--- AoC2018_09.py
+++ AoC2018_09.py
@@ -9,10 +9,13 @@ import collections
 players:int = 452
 last_marble_point: int = 70784
 player: int = 1
+current_marble: int = 1
 
 
 players_points = collections.defaultdict()
 marbles = collections.deque([0])
 
+
+# insert current_marble at position 1
 
 print(marbles[0])
