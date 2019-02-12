c62e01efacc303e799715336fc8dffbb3b2e156b
schne <schneider.solingen@web.de>
2019-02-12 07:35:11 +0100

    Day 9 -

--- AoC2018_09.py
+++ AoC2018_09.py
@@ -6,8 +6,10 @@ Github: StefSchneider
 
 import collections
 
-players:int = 452
+max_players:int = 452
+max_players = 5
 last_marble_point: int = 70784
+last_marble_point = 24
 player: int = 1
 current_marble: int = 1
 
@@ -18,4 +20,20 @@ marbles = collections.deque([0])
 
 # insert current_marble at position 1
 
-print(marbles[0])
+while current_marble <= last_marble_point:
+    print("current marble:", current_marble)
+    print("current player:", player)
+    if current_marble % 23 == 0:
+        marbles.rotate(8)
+        print(marbles[0])
+        marbles.popleft()
+        marbles.rotate(1)
+    else:
+        marbles.insert(1, current_marble)
+    print(marbles)
+    current_marble +=1
+    marbles.rotate(-2)
+    player += 1
+    if player > max_players:
+        player = 1
+
