ff21474da7ffb8df3228e98100f43eb77314e114
schne <schneider.solingen@web.de>
2019-02-13 22:45:14 +0100

    Day 11

---
+++ AoC2018_11.py
@@ -0,0 +1,49 @@
+"""
+Advent of Code - Day 11
+Author: Stefan Schneider
+Github: StefSchneider
+"""
+
+grid_serial_number: int = 3613 # input
+
+grid_energies: dict = {} # {[top, left]:grid_energy}
+
+class Cell():
+
+    def __init__(self, x_coordinate, y_coordinate):
+        self.coordinate = x_coordinate
+        self.coordinate = y_coordinate =
+        power_level: int = 0
+        grid_energy: int = 0
+
+    def __str__(self):
+        pass
+
+    def calculate_power_level(self, x_coordinate: int, y_coordinate: int, grid_serial_number: int) -> int:
+        self.x_coordinate = x_coordinate
+        self.y_coordinate = y_coordinate
+        self.grid_serial_number = grid_serial_number
+        rack_ID: int = 0
+        power_level: int = 0
+        hundreds_digit: int = 0
+
+        rack_ID = x_coordinate + 10 # step 1
+        power_level = rack_ID * y_coordinate # step 2
+        power_level += grid_serial_number # step 3
+        power_level *= rack_ID
+        hundreds_digit = power_level // 100 #step 4
+        power_level = int(str(hundreds_digit)[-1])
+        power_level -= 5 # step 5
+
+        return power_level
+
+
+    def calculate_grid_energy(self, x_coordinate: int, y_coordinate: int) -> int:
+        self.x_coordinate = x_coordinate
+        self.y_coordinate = y_coordinate
+        grid_energy: int = 0
+        for i in range(0, 3):
+            for j in range(0,3):
+                grid_energy += cell[i][j].power_level
+
+        return grid_energy
