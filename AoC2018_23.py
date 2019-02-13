1cb0e9b14ed6548d5c0f18197e4412a6e25bfa8d
schne <schneider.solingen@web.de>
2019-02-13 18:51:12 +0100

    Day 23 - just part one

--- AoC2018_23.py
+++ AoC2018_23.py
@@ -1,10 +1,10 @@
-'''
+"""
 Advent of Code - Day 23
 Author: Stefan Schneider
-Github: stefschneider1970
-'''
+Github: StefSchneider
+"""
 
-path_file: str = 'AoC2018_23_data_nanobots.txt'
+path_file: str = "AoC2018_23_data_nanobots.txt"
 nanobots: list = [] # creates an empty list for all nanobots
 max_radius: int = 0
 bot_number_max_radius: int = 0
@@ -24,30 +24,30 @@ class Nanobot:
         return self.number, self.x_position, self.y_position, self.z_position, self.bot_radius
 
 
-with open(path_file, 'r') as data_file:
+with open(path_file, "r") as data_file:
     # use file with data for nanobots
     number_bot: int = 0
     for line in data_file: # load nanobots from data file
         number_bot += 1
-        positions, radius = line.split(', r=')
-        radius = int(radius.strip(''))
-        positions = positions.lstrip('pos=<')
-        positions = positions.rstrip('>')
+        positions, radius = line.split(", r=")
+        radius = int(radius.strip(""))
+        positions = positions.lstrip("pos=<")
+        positions = positions.rstrip(">")
         positions = positions.strip()
-        x_position, y_position, z_position = positions.split(',')
+        x_position, y_position, z_position = positions.split(",")
         nanobot = Nanobot(number_bot, int(x_position), int(y_position), int(z_position), radius)
         nanobots.append(nanobot)
     print('Nanobots included: ', number_bot)
 
 # ---------- PART ONE ----------
 
-print('Starting part one...\r')
+print("Starting part one...\r")
 
 for count in range(0, number_bot): # find bot with maximum radius
     if nanobots[count].bot_radius > max_radius:
         max_radius = nanobots[count].bot_radius
         bot_number_max_radius = nanobots[count].number
-        print('New maximum radius: ', max_radius, 'of bot: ',bot_number_max_radius)
+        print("New maximum radius:", max_radius, "of bot:", bot_number_max_radius)
 
 for count in range(0, number_bot): # counts every nanobot in range
     if nanobots[count].sum_positions <= max_radius:
@@ -56,14 +56,14 @@ for count in range(0, number_bot): # counts every nanobot in range
 if nanobots[bot_number_max_radius].sum_positions > max_radius: # includes nanobot with maximum radius
     number_bots_in_range += 1
 
-print('Maximum Radius: ', max_radius, ' of bot:', bot_number_max_radius)
-print('Bots in range: ', number_bots_in_range)
-print('\r')
+print("Maximum Radius:", max_radius, " of bot:", bot_number_max_radius)
+print("Bots in range:", number_bots_in_range)
+print("\r")
 
 
 # ---------- PART TWO ---------
 
-print('Starting part two...\r')
+print("Starting part two...\r")
 
 origin = Nanobot(0, 0, 0, 0, 0) # save own coordinates at 0, 0, 0 by using class Nanobot
 
