e79dc07e2da4e22c11e39a73eff3c47f21ecde63
schne <schneider.solingen@web.de>
2019-02-19 21:03:02 +0100

    Day 11 - final solution complete

--- AoC2018_11.py
+++ AoC2018_11.py
@@ -9,8 +9,9 @@ import numpy
 grid_serial_number: int = 3613 # input
 grid_energies: dict = {} # {[top, left]:grid_energy}
 max_grid: tuple = (0 ,0, 0)
+square_3_grid: tuple = (0 ,0, 0)
+square_x_grid: tuple = (0, 0, 0)
 max_grid_energy: int = 0
-
 
 
 def calculate_power_level(x_coordinate: int, y_coordinate: int, grid_serial_number: int) -> int:
@@ -35,12 +36,16 @@ def calculate_grid_energy(window) -> int:
 
     return grid_energy
 
+print("Calculate power level for each cell...\r")
 
-grid = numpy.empty((300, 300), int)
+grid = numpy.empty((300, 300), int) # initialize grid
 for y in range(0, 300):
     for x in range(0, 300):
-        grid[x][y] = calculate_power_level(x+1, y+1, grid_serial_number)
+        grid[x][y] = calculate_power_level(x+1, y+1, grid_serial_number) # calculate power level for each cell
+print(grid, "\r")
 
+print("\r")
+print("Search for maximum total energy square...\r")
 
 for size in range(1, 301):
     grid_energies = {}
@@ -49,9 +54,14 @@ for size in range(1, 301):
             window_energy = calculate_grid_energy(grid[j:j+size, i:i+size])
             grid_energies[(j+1,i+1), size] = window_energy
     max_grid = [(k, grid_energies[k]) for k in sorted(grid_energies, key=grid_energies.get, reverse=True)][0]
-    print(max_grid)
+    if size == 3:
+        square_3_grid = max_grid[0]
     if max_grid[1] > max_grid_energy:
         print("New high energy:", max_grid)
         max_grid_energy = max_grid[1]
+        square_x_grid = max_grid
 
-
+print("\r")
+print("Results:\r")
+print("Largest total power of 3x3 square:", square_3_grid[0], "\r")
+print("Largest total power of any grid at square:", (square_x_grid[0])[0], "at square size:", (square_x_grid[0])[1])
